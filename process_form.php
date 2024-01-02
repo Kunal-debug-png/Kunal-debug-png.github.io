e13395f00bd8cde7f4891f581343addd440c9fb2
Kunal_codes <66221311+Kunal-debug-png@users.noreply.github.com>
2024-01-03 00:55:04 +0530

    Update process_form.php
    contact for php

--- process_form.php
+++ process_form.php
@@ -1,18 +1,24 @@
-
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-
-    $to = "kunalchhablani14@gmail.com"; // Your email address
-    $subject = "New message from your website";
-
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-
+    
+    // Email parameters
+    $to = "kunalchhablani14@gmail.com"; // Replace with your email address
+    $subject = "New Message from Your Website";
+    $headers = "From: $name <$email>";
+    
+    // Compose the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Message:\n$message\n";
+    
     // Send email
-    mail($to, $subject, $email_content);
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Oops! Something went wrong.";
+    }
 }
 ?>
